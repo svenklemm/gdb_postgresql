1236c78ff414bbfd81335781bb415711af4cb715
Sven Klemm <sven@timescale.com>
2024-10-22 11:14:47 +0200

    Limit nesting when printing nested objects

--- gdb_postgresql/base.py
+++ gdb_postgresql/base.py
@@ -10,6 +10,7 @@ class Registry(type):
             Registry.printers[name] = cls
         super(Registry, cls).__init__(name, bases, clsdict)
 
+NESTING_LEVEL = 0
 
 class PgObject(object, metaclass=Registry):
     prefix = ""
@@ -24,57 +25,64 @@ class PgObject(object, metaclass=Registry):
         self.val = val.cast(self.pgtype)
 
     def to_string(self):
+        global NESTING_LEVEL
         data = []
-        for f in self.pgtype.fields():
-            field_name = f.name
-            try:
-                if hasattr(self.__class__, "skipped_fields") and field_name in getattr(
-                    self.__class__, "skipped_fields"
-                ):
-                    continue
+        NESTING_LEVEL+=1
+        if NESTING_LEVEL < 2:
+            for f in self.pgtype.fields():
+                field_name = f.name
+                try:
+                    if hasattr(self.__class__, "skipped_fields") and field_name in getattr(
+                        self.__class__, "skipped_fields"
+                    ):
+                        continue
 
-                if (
-                    hasattr(self.__class__, "skipped_zero")
-                    and field_name in getattr(self.__class__, "skipped_zero")
-                    and self.val[f] == 0
-                ):
-                    continue
+                    if (
+                        hasattr(self.__class__, "skipped_zero")
+                        and field_name in getattr(self.__class__, "skipped_zero")
+                        and self.val[f] == 0
+                    ):
+                        continue
 
-                if field_name == "location" and self.val[f] >= -1:
-                    continue
+                    if field_name == "location" and self.val[f] >= -1:
+                        continue
 
-                val = self.val[f]
-                str_val = str(val)
+                    val = self.val[f]
+                    str_val = str(val)
 
-                if str_val == "0x0":
-                    continue
+                    if str_val == "0x0":
+                        continue
 
-                if gdb.default_visualizer(val):
-                    str_val = gdb.default_visualizer(val).to_string()
-                elif str(f.type) == "void *":
-                    pass
-                elif str(f.type) == "char *" and str_val != "0x0":
-                    str_val = repr(val.string())
-                elif (
-                    str(f.type)[-2:] == " *"
-                    and str_val != "0x0"
-                    and gdb.default_visualizer(val.dereference())
-                ):
-                    str_val = gdb.default_visualizer(val.dereference()).to_string()
-                elif str(f.type) == "Relids" and str_val != "0x0":
-                    str_val = gdb.default_visualizer(val.dereference()).to_string()
-                elif hasattr(self.__class__, "lookup_" + f.name):
-                    str_val = getattr(self.__class__, "lookup_" + f.name)(self, val)
+                    if gdb.default_visualizer(val):
+                        str_val = gdb.default_visualizer(val).to_string()
+                    elif str(f.type) == "void *":
+                        pass
+                    elif str(f.type) == "char *" and str_val != "0x0":
+                        str_val = repr(val.string())
+                    elif (
+                        str(f.type)[-2:] == " *"
+                        and str_val != "0x0"
+                        and gdb.default_visualizer(val.dereference())
+                    ):
+                        str_val = gdb.default_visualizer(val.dereference()).to_string()
+                    elif str(f.type) == "Relids" and str_val != "0x0":
+                        str_val = gdb.default_visualizer(val.dereference()).to_string()
+                    elif hasattr(self.__class__, "lookup_" + f.name):
+                        str_val = getattr(self.__class__, "lookup_" + f.name)(self, val)
 
-                if self.prefix and field_name.startswith(self.prefix):
-                    field_name = f.name[len(self.prefix) :]
+                    if self.prefix and field_name.startswith(self.prefix):
+                        field_name = f.name[len(self.prefix) :]
 
-                data.append("{}={}".format(field_name, str_val))
+                    data.append("{}={}".format(field_name, str_val))
 
-            except BaseException as err:
-                # print_exception(err)
-                data.append("{}={}".format(field_name, "ERROR"))
-        return "<{} {}>".format(self.pgtype.name, ", ".join(data))
+                except BaseException as err:
+                    # print_exception(err)
+                    data.append("{}={}".format(field_name, "ERROR"))
+        NESTING_LEVEL -= 1
+        if not data:
+            return "<{}>".format(self.pgtype.name)
+        else:
+            return "<{} {}>".format(self.pgtype.name, ", ".join(data))
 
 
 class Node(PgObject):
